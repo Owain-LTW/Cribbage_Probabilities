00bf4815d9d6bca2408f1d994f53d924aed5dad7
Owain Walker <owain.ll.walker@gmail.com>
2021-09-27 23:44:27 +0100

    Adding all files for push to remote

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='OCaDetermine hthe various points totals available for a certain hand in cribbage, and the probabilities of obtaining these points values.',
+    author='Owain Walker',
+    license='',
+)
