89b22ac5892d37f219ef43f16e8272251d3b6a6a
Owain Walker <owain.ll.walker@gmail.com>
2021-10-10 20:39:09 +0100

    Now using numpy.random.choice to select card from deck

--- src/models/cribbage_calc_scores_and_probs.py
+++ src/models/cribbage_calc_scores_and_probs.py
@@ -18,22 +18,25 @@ def main():
     crib_path = '/Users/oltw/Documents/Cribbage_Probabilities/src/data/possible_cribs.csv'
     undealt_cards_path = '/Users/oltw/Documents/Cribbage_Probabilities/src/data/remaining_cards.csv'
     
-    hand1_possibilities = numpy.loadtxt(hand1_path, dtype = str)
-    hand2_possibilities = numpy.loadtxt(hand2_path, dtype = str)
-    crib_possibilities = numpy.loadtxt(crib_path, dtype = str)
-    deck = numpy.loadtxt(undealt_cards_path, dtype = str)
+    hand1_possibilities = numpy.genfromtxt(hand1_path, dtype = str)
+    hand2_possibilities = numpy.genfromtxt(hand2_path, dtype = str)
+    crib_possibilities = numpy.genfromtxt(crib_path, dtype = str)
+    deck = numpy.genfromtxt(undealt_cards_path, dtype = str)
+    
     
     print('Hand 1',hand1_possibilities)
     print('Hand 2',hand2_possibilities)
     print('Crib',crib_possibilities)
-    print('UDC',undealt_cards)
+    print('UDC',deck)
     
-    cut_card = sample(undealt_cards,1)
+    cut_card = numpy.random.choice(deck,1)
     
     print(cut_card)
     
     for hand in range (0,len(hand1_possibilities)):
+        print(hand1_possibilities[hand])
         calculate_score(hand1_possibilities[hand], cut_card)
+        
 
 
 def calculate_score(hand, cut_card):
@@ -43,12 +46,23 @@ def calculate_score(hand, cut_card):
 def calc_15s(hand, cut_card):
     card_values = [hand[3],hand[9],hand[15],hand[21]]
     #if item in list is J,Q,K,0 then make it 10
+    card_values = list(map(int, card_values))
+    score = 0
     for i in range(0,len(card_values)):
         if card_values[i] == 'J' or card_values[i] == 'Q' or card_values[i] == 'K':
             card_values[i] = '10'
-    card_values = list(map(int, card_values))
-    
-    score = 0
+        for j in range(0,len(card_values)):
+            if j <= i:
+                continue
+            if card_values[i]+card_values[j]==15:
+                score = score + 2
+            for k in range (0,len(card_values)):
+                if k <= j:
+                    continue
+                if card_values[i]+card_values[j]+card_values[k] == 15:
+                    score = score + 2
+    if card_values[0] + card_values[1] + card_values[2] + card_values[3] == 15:
+        score = score + 2
     
     print(card_values, score)
 
